f096d2ec18ee649a9b1fbddf761a150aaecb7a5e
Mike Shippy <mikeship04@gmail.com>
2022-06-08 20:43:46 -0700

    commit tree 087c5a65fe33270bc117e05f0dd92b470576fa20

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+// function superbowlWin(anArray){
+//     for(let item of anArray){
+//         if(item.result === 'W'){
+//         return(item.year)
+//         }
+//     }
+// }
+
+function superbowlWin(anArray){
+    let outcome = anArray.find(obj => {
+        return(obj.result === 'W')
+    })
+    if(outcome !== undefined) {
+        return(outcome.year)
+    }else {
+        return(undefined)
+    }
+    console.log(outcome)
+}
+
+
+
+let newArray = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+console.log(superbowlWin(newArray))
+
